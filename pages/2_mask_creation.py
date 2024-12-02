c7d4930a8f5f7af435dc9f748743c40ccada7443
Francisco Urra <urra.francisco@googlemail.com>
2024-12-03 00:57:57 +0100

    Add files via upload

--- pages/2_mask_creation.py
+++ pages/2_mask_creation.py
@@ -10,7 +10,7 @@ st.header("CIS5810 - 5 - Photography techniques")
 
 option = st.selectbox(
     "Segmentation technique:",
-    ["Felzenswald", "Clustering", "Edge", "Region", "Neural network"],
+    ["Felzenswald", "Clustering", "Edge", "Neural network"],
     index=0,
     placeholder="Choose an option"
 )
@@ -220,30 +220,82 @@ if option == "Edge":
     sobel_horizontal = np.array([np.array([1, 2, 1]), np.array([0, 0, 0]), np.array([-1, -2, -1])])
     sobel_vertical = np.array([np.array([-1, 0, 1]), np.array([-2, 0, 2]), np.array([-1, 0, 1])])
 
-    out_h = ndimage.convolve(gray, sobel_horizontal, mode='reflect')
-    out_v = ndimage.convolve(gray, sobel_vertical, mode='reflect')
-    out = out_h + out_v
+    choice = st.radio(
+        "Make your selection",
+        ["Original image", "Preprocessing image"],
+        horizontal=True  # Display horizontally instead of vertically
+    )
 
-    col1, col2, col3, col4 = st.columns(4)
+    if choice == "Original image":
 
-    with col1:
-        st.subheader("Image")
-        st.image(img)
+        out_h = ndimage.convolve(gray, sobel_horizontal, mode='reflect')
+        out_v = ndimage.convolve(gray, sobel_vertical, mode='reflect')
+        out = out_h + out_v
 
-    with col2:
-        st.subheader("horizontal")
-        normalized = cv2.normalize(out_h, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        st.image(normalized)
+        col1, col2, col3, col4 = st.columns(4)
 
-    with col3:
-        st.subheader("vertical")
-        normalized = cv2.normalize(out_v, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        st.image(normalized)
+        with col1:
+            st.subheader("Image")
+            st.image(img)
 
-    with col4:
-        st.subheader("both")
-        normalized = cv2.normalize(out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        st.image(normalized)
+        with col2:
+            st.subheader("horizontal")
+            normalized = cv2.normalize(out_h, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
+
+        with col3:
+            st.subheader("vertical")
+            normalized = cv2.normalize(out_v, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
+
+        with col4:
+            st.subheader("both")
+            normalized = cv2.normalize(out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
+
+    if choice == "Preprocessing image":
+
+        st.write("The image was preprocessed to try to decrease the edges in the tree and keep only the edges in the car using grayscale, gaussian blur, dilation, morphological closing")
+
+        gray = rgb2gray(img)
+        # Apply Gaussian blur to the grayscale image
+        blur = cv2.GaussianBlur(gray, (25, 25), 0)
+        # Apply dilation to the blurred image
+        dilated = cv2.dilate(blur, np.ones((3, 3)))
+        # Apply morphological closing to the dilated image
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+        closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
+
+        out_h = ndimage.convolve(closing, sobel_horizontal, mode='reflect')
+        out_v = ndimage.convolve(closing, sobel_vertical, mode='reflect')
+        out = out_h + out_v
+
+
+
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            st.subheader("Image")
+            st.image(img)
+
+        with col2:
+            st.subheader("Prep")
+            st.image(closing )
+
+        with col3:
+            st.subheader("h")
+            normalized = cv2.normalize(out_h, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
+
+        with col4:
+            st.subheader("v")
+            normalized = cv2.normalize(out_v, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
+
+        with col5:
+            st.subheader("both")
+            normalized = cv2.normalize(out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            st.image(normalized)
 
 if option == "Region":
 
@@ -293,7 +345,7 @@ if option == "Region":
 
 if option == "Neural network":
 
-    st.subheader("Segmentation with neural networks - rembg")
+    st.subheader("Segmentation with neural networks - Rembg")
 
     from rembg import remove
     from PIL import Image
